dbf3749c36767e031b378246ab7f4dc17fee15f1
millz60 <Matt@Matts-MacBook-Pro.local>
2016-06-22 18:06:30 -0500

    Updated Visually and Functionally
    Switched button sides, added custom buttons, added a method to handle
    the stepper value, added min and max range text fields.

--- StepperViewControl/ViewController.m
+++ StepperViewControl/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "StepperView.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
 
@@ -18,7 +19,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.view.backgroundColor = [UIColor blackColor];
+    self.view.backgroundColor = [UIColor whiteColor];
     
     
     // Create stepperView Object and set its center to the center of the view
@@ -26,38 +27,60 @@
     stepperView.frame = CGRectMake(0, 0, 300, 50);
     stepperView.backgroundColor = [UIColor redColor];
     stepperView.center = self.view.center;
+    stepperView.layer.cornerRadius = 5;
+    stepperView.layer.masksToBounds = YES;
+
+    
     
     // Add stepperView to root view
     [self.view addSubview:stepperView];
     
     
-    // Create plusButton view and then "+" label inside of it and add to stepperView
+    // Create plusButton view and then "+" button inside of it and add to stepperView
     UIView *plusButton = [[UIView alloc] init];
-    plusButton.frame = CGRectMake(0, 0, 50, 50);
-    plusButton.backgroundColor = [UIColor blueColor];
+    plusButton.frame = CGRectMake(250, 0, 50, 50);
+    plusButton.backgroundColor = [UIColor blackColor];
     
-    UILabel *plus = [[UILabel alloc] init];
-    plus.text = @"+";
+    UIButton *plus = [[UIButton alloc] init];
+    [plus setTitle:@"+" forState:UIControlStateNormal];
     plus.frame = CGRectMake(12.5, 12.5, 25, 25);
     plus.backgroundColor = [UIColor redColor];
-    plus.textAlignment = NSTextAlignmentCenter;
-    plus.font = [UIFont fontWithName:@"HelveticaNeue" size:22.0];
+    plus.titleLabel.textAlignment = NSTextAlignmentCenter;
+    plus.titleLabel.font = [UIFont fontWithName:@"AmericanTypewriter-Bold" size:22.0];
+    plus.layer.cornerRadius = 5;
+    plus.layer.masksToBounds = YES;
+    plus.layer.shadowColor = [[UIColor blackColor] CGColor];
+    plus.layer.shadowOffset = CGSizeMake(2.0f, 2.0f);
+    plus.layer.shadowRadius = 5.0f;
+    plus.layer.shadowOpacity = 0.5f;
     [plusButton addSubview:plus];
     
     [stepperView addSubview:plusButton];
     
-    // Create minusButton view and then "-" label inside of it and add to stepperView
+    // Create minusButton view and then "-" button inside of it and add to stepperView
     UIView *minusButton = [[UIView alloc] init];
-    minusButton.frame = CGRectMake(250, 0, 50, 50);
-    minusButton.backgroundColor = [UIColor blueColor];
+    minusButton.frame = CGRectMake(0, 0, 50, 50);
+    minusButton.backgroundColor = [UIColor blackColor];
     
-    UILabel *minus = [[UILabel alloc] init];
-    minus.text = @"-";
+    UIButton *minus = [[UIButton alloc] init];
     minus.frame = CGRectMake(12.5, 12.5, 25, 25);
     minus.backgroundColor = [UIColor redColor];
-    minus.textAlignment = NSTextAlignmentCenter;
-    minus.font = [UIFont fontWithName:@"HelveticaNeue" size:22.0];
+    [minus setTitle:@"-" forState:UIControlStateNormal];
+    minus.titleLabel.textAlignment = NSTextAlignmentCenter;
+    minus.titleLabel.font = [UIFont fontWithName:@"AmericanTypewriter-Bold" size:22.0];
+    minus.layer.cornerRadius = 5;
+    minus.layer.masksToBounds = YES;
+    minus.layer.shadowColor = [[UIColor blackColor] CGColor];
+    minus.layer.shadowOffset = CGSizeMake(1.0f, 1.0f);
+    minus.layer.shadowRadius = 3.0f;
+    minus.layer.shadowOpacity = 1.0f;
+    
+    
     [minusButton addSubview:minus];
+
+    [minus addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [plus addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+
     
     [stepperView addSubview:minusButton];
     
@@ -65,21 +88,43 @@
     // Add a labelView and label in the center to display current result and add it to stepperView
     UIView *labelView = [[UIView alloc] init];
     labelView.frame = CGRectMake(60, 5, 180, 40);
-    labelView.backgroundColor = [UIColor yellowColor];
+    labelView.backgroundColor = [UIColor whiteColor];
     
     
-    UILabel *currentNumber = [[UILabel alloc] init];
+    currentNumber = [[UILabel alloc] init];
     currentNumber.frame = CGRectMake(30, 5, 120, 30);
     currentNumber.backgroundColor = [UIColor redColor];
     currentNumber.text = @"0";
     currentNumber.textAlignment = NSTextAlignmentCenter;
-    currentNumber.font = [UIFont fontWithName:@"HelveticaNeue" size:22.0];
+    currentNumber.font = [UIFont fontWithName:@"AmericanTypewriter-Bold" size:22.0];
     [labelView addSubview:currentNumber];
     
     [stepperView addSubview:labelView];
     
 
     
+    int screenWidth = self.view.frame.size.width;
+    int screenHeight = self.view.frame.size.height;
+    
+    
+    minRange = [[UITextField alloc] initWithFrame:CGRectMake((screenWidth/4)-33, (screenHeight/4)*2.5-20, 66, 40)];
+    minRange.text = @"-10";
+    minRange.textColor = [UIColor redColor];
+    minRange.backgroundColor = [UIColor lightGrayColor];
+    minRange.layer.cornerRadius = 5;
+    minRange.layer.masksToBounds = YES;
+    minRange.textAlignment = NSTextAlignmentCenter;
+    
+    maxRange = [[UITextField alloc] initWithFrame:CGRectMake(((screenWidth/4)*3)-33, (screenHeight/4)*2.5-20, 66, 40)];
+    maxRange.text = @"10";
+    maxRange.textColor = [UIColor redColor];
+    maxRange.backgroundColor = [UIColor lightGrayColor];
+    maxRange.layer.cornerRadius = 5;
+    maxRange.layer.masksToBounds = YES;
+    maxRange.textAlignment = NSTextAlignmentCenter;
+    
+    [self.view addSubview:maxRange];
+    [self.view addSubview:minRange];
     
     
 }
@@ -88,5 +133,44 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+-(void) buttonPressed: (UIButton *) sender{
+    
+    NSString *operation = sender.titleLabel.text;
+    NSInteger labelValue = currentNumber.text.integerValue;
+    
+    NSInteger max = maxRange.text.integerValue;
+    NSInteger min = minRange.text.integerValue;
+    
+    
+    if ([operation isEqualToString:@"+"] && labelValue < max){
+        currentNumber.text = [NSString stringWithFormat:@"%ld",(labelValue+1)];
+        
+    }
+    else if ([operation isEqualToString:@"-"] && labelValue > min){
+        currentNumber.text = [NSString stringWithFormat:@"%ld",(labelValue-1)];
+    }
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
