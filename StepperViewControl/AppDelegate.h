0716be5ba498144bad9fee0ce9de3b91f50af5dc
millz60 <Matt@matts-macbook-pro.local>
2016-06-22 12:27:35 -0500

    Initial Commit

---
+++ StepperViewControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StepperViewControl
+//
+//  Created by Matt Milner on 6/22/16.
+//  Copyright © 2016 Matt Milner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
